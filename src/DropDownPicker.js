845f2b1798a027fe652d53498e68a648695badb9
beelee-the-bee <hugo.vast@gmail.com>
2022-02-23 12:30:24 +0100

    [ADD] init project

---
+++ src/DropDownPicker.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import {
+  // Animated,
+  StyleSheet, Text, TouchableOpacity, View,
+} from 'react-native';
+import FlatListSelector from './FlatListSelector';
+
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    borderRadius: 40,
+    paddingHorizontal: 20,
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  inputFocus: {
+    borderWidth: 1,
+  },
+  result: {
+    fontSize: 17,
+  },
+});
+
+export default function DropDownPicker({ resultObject, data, placeholder }) {
+  const [isDeployed, setIsDeployed] = useState(false);
+  const [result, setResult] = resultObject;
+
+  // const arrowAnim = useRef(new Animated.Value(0)).current;
+  // const rotateDown = () => {
+  //   Animated.timing(arrowAnim, {
+  //     toValue: 3.14159,
+  //     duration: 150,
+  //     useNativeDriver: true,
+  //   }).start();
+  // };
+
+  // const rotateUp = () => {
+  //   Animated.timing(arrowAnim, {
+  //     toValue: 0,
+  //     duration: 150,
+  //     useNativeDriver: true,
+  //   }).start();
+  // };
+
+  function getInputStyle() {
+    let style = styles.input;
+
+    if (isDeployed) {
+      style = { ...style, ...styles.inputFocus };
+    }
+    return style;
+  }
+
+  function handleDeployed() {
+    if (isDeployed) {
+      rotateDown();
+    } else {
+      rotateUp();
+    }
+  }
+
+  useEffect(() => {
+    handleDeployed();
+  }, [isDeployed])
+
+  
+  return (
+    <View>
+      <TouchableOpacity style={getInputStyle()} onPress={() => setIsDeployed(!isDeployed)}>
+        <Text style={styles.result}>{result?.name || result || placeholder}</Text>
+        {/* <Animated.View style={{ transform: [{ rotate: arrowAnim }] }}>
+          <IconFactory kind="polygon" scale={1.3} />
+        </Animated.View> */}
+      </TouchableOpacity>
+      {
+        isDeployed
+        && (
+        <FlatListSelector
+          setIsDeployed={setIsDeployed}
+          displayableData={data}
+          setValue={setResult}
+        />
+        )
+      }
+    </View>
+  );
+}
---
+++ src/FlatListSelector.js
@@ -0,0 +1,77 @@
+import {
+    FlatList, StyleSheet, Text, TouchableOpacity, View,
+  } from 'react-native';
+  import React from 'react';
+  
+  const styles = StyleSheet.create({
+    flatList: {
+      flexShrink: 0,
+      maxHeight: 200,
+      flexGrow: 0,
+      width: '90%',
+      alignSelf: 'center',
+      borderBottomRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      // @TODO add shadow,
+    },
+    container: {
+      backgroundColor: 'white',
+      paddingTop: 10,
+      borderBottomRightRadius: 20,
+      borderBottomLeftRadius: 20,
+    },
+    title: {
+      fontFamily: 'Ubuntu',
+      fontSize: 16,
+      marginLeft: 10,
+      maxWidth: '80%',
+    },
+    titleContainer: {
+      paddingHorizontal: 20,
+      marginVertical: 10,
+    },
+    titleAndDotContainer: {
+      paddingHorizontal: 20,
+      marginVertical: 10,
+      flexDirection: 'row',
+    },
+    dot: {
+      width: 20,
+      height: 20,
+      borderRadius: 10,
+    },
+    headerText: {
+      fontFamily: 'Ubuntu',
+      fontSize: 18,
+      paddingHorizontal: 20,
+    },
+  });
+  
+  export default function FlatListSelector({
+    displayableData, setValue, setIsDeployed,
+  }) {
+
+    return (
+      <FlatList
+        style={styles.flatList}
+        contentContainerStyle={styles.container}
+        nestedScrollEnabled
+        data={displayableData}
+        keyExtractor={(item) => `${item.id || item.name || item}`}
+        renderItem={({ item }) => {
+            <TouchableOpacity
+            onPress={() => {
+              setValue(item?.name || item);
+              setIsDeployed(false);
+            }}
+            style={styles.titleContainer}
+          >
+            <Text style={styles.title}>
+              {item.name || item}
+            </Text>
+          </TouchableOpacity>
+        }}
+      />
+    );
+  }
+  
